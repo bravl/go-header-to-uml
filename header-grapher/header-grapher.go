ca8b64abc5c69b4428b59adee24c5240d915aef7
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2018-11-30 13:39:57 +0100

    Variable regex updated and implemented
    Debug print implemented
    matchStruct() feature complete, everything stored in gStructs
    
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

--- header-grapher/header-grapher.go
+++ header-grapher/header-grapher.go
@@ -12,7 +12,7 @@ type variable_node struct {
 	variableName string
 	arrayDepends []string
 	arrayDepth   int
-	isArray      bool
+	isStruct     bool
 }
 type struct_node struct {
 	varType string
@@ -26,8 +26,9 @@ var structRegex = regexp.MustCompile(`(?ms)^ ?struct .*?\{(.*?)};`)
 var structNameRegex = regexp.MustCompile(`(?ms)^ ?struct ([^\s]+).?{`)
 var enumRegex = regexp.MustCompile(`(?ms)^ ?struct ([^\s]+) ?\{.?enum ([^\s]+) ?{(.*?)};.?};`)
 var enumNameRegex = regexp.MustCompile(`(?ms)^ ?enum(.*?)\{`)
-var variableRegex = regexp.MustCompile(`(?ms)^ ?([^\s]+) ?([^\s]+);`)
+var variableRegex = regexp.MustCompile(`(?ms)^.?([^\s]+) ?([^\s]+) ?([^\s]+);`)
 var bracketRegex = regexp.MustCompile(`(?ms)\[(.*?)\]`)
+var isStructVarRegex = regexp.MustCompile(`(?ms)^.?struct ?([^\s]+) ?([^\s]+)[;\[]`)
 
 func standardizeSpaces(s string) string {
 	return strings.Join(strings.Fields(s), " ")
@@ -46,22 +47,49 @@ func prepareFile(fileContents string) string {
 	return strings.Join(lines, "\n")
 }
 
+func printVar(v *variable_node) {
+	fmt.Println(v.variableName + " : " + v.variableType)
+	fmt.Println("Struct: ", v.isStruct)
+	fmt.Println("Array Depth: ", v.arrayDepth)
+	fmt.Println("Array Params: ", v.arrayDepends)
+	fmt.Println()
+}
+
 func matchStructs(fileContents string) {
 	tFile := enumRegex.ReplaceAllLiteralString(fileContents, "")
 	names := structNameRegex.FindAllString(tFile, -1)
 	structs := structRegex.FindAllStringSubmatch(tFile, -1)
-	fmt.Println(len(names))
-	fmt.Println(len(structs))
 	for index, str := range names {
 		str = strings.Replace(str, "\n", "", -1)
 		str = strings.TrimSpace(str)
-		fmt.Println(strings.Replace(str, "{", "", -1))
 
 		tmpStruct := new(struct_node)
 
 		for _, tmp := range strings.Split(structs[index][1], "\n") {
+			tmpVar := new(variable_node)
+
 			arrayParams := bracketRegex.FindAllString(tmp, -1)
-			fmt.Println(arrayParams)
+			tmp = bracketRegex.ReplaceAllLiteralString(tmp, "")
+
+			variable := variableRegex.FindString(tmp)
+			if len(variable) == 0 {
+				continue
+			}
+
+			tmpVar.arrayDepends = arrayParams
+			tmpVar.arrayDepth = len(arrayParams)
+
+			tmpVar.isStruct = isStructVarRegex.MatchString(variable)
+			variables := strings.Split(variable, " ")
+			if tmpVar.isStruct {
+				tmpVar.variableType = variables[1]
+				tmpVar.variableName = variables[2]
+			} else {
+				tmpVar.variableType = variables[0]
+				tmpVar.variableName = variables[1]
+			}
+
+			tmpStruct.vars = append(tmpStruct.vars, tmpVar)
 		}
 		gStructs = append(gStructs, tmpStruct)
 	}
@@ -72,7 +100,11 @@ func RunGrapher(file, tool string) bool {
 	fmt.Println("Running grapher")
 	bFile, _ := ioutil.ReadFile(file)
 	sFile := string(bFile)
-	//fmt.Println(prepareFile(sFile))
 	matchStructs(prepareFile(sFile))
+	for _, str := range gStructs {
+		for _, v := range str.vars {
+			printVar(v)
+		}
+	}
 	return true
 }
